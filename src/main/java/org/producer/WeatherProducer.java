600d9c2ffdf2393696814c0203ba5635378bab38
Nastya <vnedostupa@yandex.ru>
2025-07-15 01:53:21 +0300

    Добавление KafkaConsumer

--- src/main/java/org/producer/WeatherProducer.java
+++ src/main/java/org/producer/WeatherProducer.java
@@ -1,5 +1,11 @@
 package org.producer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.model.WeatherCondition;
 import org.model.WeatherData;
 
@@ -7,7 +13,11 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Properties;
 import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static java.time.LocalDateTime.now;
 
@@ -25,17 +35,44 @@ public class WeatherProducer {
         WeatherCondition randomCondition = WeatherCondition.getRandom();
 
         int daysBack = random.nextInt(7);
-        LocalDateTime eventDate = now().minusDays(daysBack).with(LocalTime.now().withNano(0));
+        LocalDateTime eventDate = now().minusDays(daysBack).with(LocalTime.MIDNIGHT);
 
         WeatherData weatherData = new WeatherData(randomTemperature, randomCondition, randomCity, eventDate);
 
         return weatherData;
     }
     public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        WeatherData weatherData = generateRandomWeatherData();
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        ObjectMapper mapper = new ObjectMapper();
+        String topic = "weather-topic";
 
-        System.out.println(weatherData);
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+        scheduler.scheduleAtFixedRate(() -> {
+            try {
+                WeatherData weatherData = generateRandomWeatherData();
+                String json = mapper.writeValueAsString(weatherData);
+
+                producer.send(new ProducerRecord<>(topic, json));
+                System.out.println("Отправлено: " + weatherData);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, 0, 2, TimeUnit.SECONDS);
+
+        // Обработка остановки программы (Ctrl+C)
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Останавливаем producer...");
+            scheduler.shutdownNow();
+            producer.close();
+            System.out.println("Producer остановлен.");
+        }));
     }
 
 }
